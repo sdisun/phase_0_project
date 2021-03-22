61e3aa2e192a78e27d9c2b596ddac90205b69b87
Neil <zooming.in@hotmail.com>
2021-03-22 23:55:50 +1100

    updates

---
+++ index.js
@@ -0,0 +1,6 @@
+function changeColour() {
+    let text = document.getElementById("button").style.backgroundSize = 120%;
+
+function returnOriginal() {
+    let text = document.getElementById("button").style.backgroundSize = "";
+}
